74800d0128d6298fe781485526354c5a56d6a7f1
lurdharry <azeezhammed57@gmail.com>
2020-02-15 22:52:29 +0100

    Created Dummy data for Users in Chat screen

---
+++ src/Components/userDB.js
@@ -0,0 +1,50 @@
+export const UserDetails=[
+    {
+        name:'Josie Vega',
+        status:'Online',
+        img:require('../assets/images/Users/user1.png'),
+        notification:[{},{},{},{},{}]
+    },
+    {
+        name:'Gerald French',
+        status:'Offline',
+        img:require('../assets/images/Users/user2.png'),
+                notification:[{},{}]
+
+    },
+    {
+        name:'Alberta Hawkins',
+        status:'Online',
+        img:require('../assets/images/Users/user3.png'),
+                notification:[{},{},{},{}]
+
+    },
+    {
+        name:'Paul Walker',
+        status:'Away',
+        img:require('../assets/images/Users/user4.png'),
+                notification:[]
+
+    },
+    {
+        name:'Jenny Stevenson',
+        status:'Online',
+        img:require('../assets/images/Users/user5.png'),
+                notification:[{},{},{}]
+
+    },
+    {
+        name:'Gary Sullivan',
+        status:'Away',
+        img:require('../assets/images/Users/user6.png'),
+                notification:[{},{}]
+
+    },
+    {
+        name:'Nettie Vasquez',
+        status:'Offline',
+        img:require('../assets/images/Users/user7.png'),
+                notification:[{},]
+
+    },
+]
