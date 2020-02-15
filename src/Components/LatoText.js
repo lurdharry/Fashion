caebadabf35920b9597ef9e929fecaf95c92a34c
lurdharry <azeezhammed57@gmail.com>
2020-02-15 22:53:25 +0100

    Created Custom Text

--- src/Components/LatoText.js
+++ src/Components/LatoText.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import {Text} from 'react-native'
 
-export const LatoText =()=>{
+export const LatoText =props=>{
     return (
-        <Text {...props} style={[props.style,{fontFamily:'Lato-rg'}]}>
+        <Text {...props} style={[props.style,{fontFamily:'Proxima-rg'}]}>
             {props.children}
         </Text>
     )
