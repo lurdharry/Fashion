7971639d656039a6f23f3a0477a0eab50dd45423
lurdharry <azeezhammed57@gmail.com>
2020-02-18 08:46:02 +0100

    Created Dummy data for Dresses Category

---
+++ src/Components/DressData.js
@@ -0,0 +1,38 @@
+export const DressData =[
+    {
+        name:'T-shirt',
+        img:require('../assets/images/shirt3.png'),
+        price:'38',
+        category:'Dresses',
+    },
+    {
+        name:'Blank Top',
+        img:require('../assets/images/shirt6.png'),
+        price:'38',
+        category:'Dresses',
+    },
+    {
+        name:'Gown',
+        img:require('../assets/images/shirt.png'),
+        price:'38',
+        category:'Dresses',
+    },
+    {
+        name:'Armless Top',
+        img:require('../assets/images/shirt5.png'),
+        price:'38',
+        category:'Dresses',
+    },
+    {
+        name:'Sweat Shirt',
+        img:require('../assets/images/shirt4.png'),
+        price:'38',
+        category:'Dresses',
+    },
+    {
+        name:'Indian Dress',
+        img:require('../assets/images/shirt6.png'),
+        price:'38',
+        category:'Dresses',
+    },
+]
